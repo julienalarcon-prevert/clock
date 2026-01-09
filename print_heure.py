8c5f208a68e87b7a7050e45910b8c4ce965f0399
Julien-13 <julien.alarcon-prevert@laplateforme.io>
2026-01-09 09:58:04 +0100

    Pre final version
    change the order of the instructions.
    
    First alarm
    Second set up time

--- print_heure.py
+++ print_heure.py
@@ -15,7 +15,7 @@ def get_hour_sys():
     return (hour_now.hour, hour_now.minute, hour_now.second)
 
 def next(hour):
-    h , m , s = hour
+    h, m , s = hour
     s = s + 1
     if s == 60:
         s = 0 
@@ -44,10 +44,11 @@ def choose_hour():
                 return (h, m, s)
             else : 
                 print("ERREUR : Value impossible ! please respect the instructions")
+                
         except ValueError:
             print("Erreur : Enter only numbers")
 
-def main(hour_start, hour_alarme):
+def main( hour_alarme,hour_start):
     hour_actuelle = hour_start
     print("\nClock is ready press 'q' for stop the clock.")
     
@@ -72,19 +73,31 @@ def main(hour_start, hour_alarme):
                     print("\nPress 'q' for leave.")
                 else : 
                     hour_alarme = None
-                    print("Alarm desable")
+                    print("Alarme désactivée")
                     print("\nPress 'q' for leave.")
         
         hour_actuelle = next(hour_actuelle)
         time.sleep(1)
     
-    print("\nSTOP PROGRAM")
-
-def start_menu ():
+    print("\nArrêt du programme.")
+def alarm_menu ():
     while True:
         print("\n Hello and welcome in the grandma's clock, here we will have a clock in real time") 
         time.sleep(5)
-        print("\n Firstly, you will have the choice of using the current time or customizing it.")
+        print("\n Firstly,you will have the choice of setting an alarm or not.")
+        time.sleep(3)
+        activer_alarme = input("Do you want to set a clock? (o-n): ").lower()
+
+        if activer_alarme == "o":
+            print("--- SET UP ALARME ---")
+            return choose_hour()
+        elif activer_alarme == "n":
+            return None
+        else :
+            print("ERROR : Answer with 'o' or 'n'.")            
+def start_menu ():
+    while True:
+        print("\n Now, you will have the choice of using the current time or customizing it.")
         time.sleep(3)
         print("\n1. Using the system time | 2. Using a custom time")
         choix = input("Your choise : ")
@@ -97,26 +110,13 @@ def start_menu ():
         else : 
             print("ERROR : Enter only 1 or 2")
 
-def alarm_menu ():
-    while True:
-        print("\n Now you will have the choice of setting an alarm or not.")
-        time.sleep(3)
-        activer_alarme = input("Do you want to set a clock? (o-n): ").lower()
-
-        if activer_alarme == "o":
-            print("--- SET UP ALARME ---")
-            return choose_hour()
-        elif activer_alarme == "n":
-            return None
-        else :
-            print("ERROR : Answer with 'o' or 'n'.")            
 
 def start_program():
     alarm = alarm_menu()
     start = start_menu()
     
     
-    main(start, alarm)
+    main(alarm,start )
 
 if __name__ == "__main__":
     start_program()
